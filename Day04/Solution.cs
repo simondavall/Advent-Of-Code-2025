e7574caaab8adba5cc9a6d5f9968053a1daca306
Simon Da Vall <simon.davall@gmail.com>
2025-12-04 10:55:17 +0000

    Day04

---
+++ Day04/Solution.cs
@@ -0,0 +1,85 @@
+﻿namespace Day04;
+
+internal static partial class Program {
+  private const string Title = "\n## Day 4: Printing Department ##";
+  private const string AdventOfCode = "https://adventofcode.com/2025/day/4";
+  private const long ExpectedPartOne = 1604;
+  private const long ExpectedPartTwo = 9397;
+
+  private static readonly (int dx, int dy)[] _directions = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)];
+
+  private static long PartOne(string[] data) {
+    var grid = GetGrid(data);
+    var height = data.Length;
+    var width = data[0].Length;
+
+    long tally = 0;
+
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        if (grid[y][x] != '@')
+          continue;
+        var counter = GetCount(x, y, height, width, grid);
+        if (counter < 4)
+          tally++;
+      }
+    }
+
+    return tally;
+  }
+
+  private static long PartTwo(string[] data) {
+    var grid = GetGrid(data);
+    var height = data.Length;
+    var width = data[0].Length;
+    List<(int x, int y)> toBeRemoved = [];
+
+    long tally = 0;
+
+    while (true) {
+      toBeRemoved.Clear();
+      for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+          if (grid[y][x] != '@')
+            continue;
+          var counter = GetCount(x, y, height, width, grid);
+          if (counter < 4)
+            toBeRemoved.Add((x, y));
+        }
+      }
+      if (toBeRemoved.Count == 0)
+        break;
+
+      tally += toBeRemoved.Count;
+      foreach (var (x, y) in toBeRemoved)
+        grid[y][x] = '.';
+    }
+
+    return tally;
+  }
+
+  private static int GetCount(int x, int y, int height, int width, char[][] grid) {
+    var counter = 0;
+    foreach (var (dx, dy) in _directions) {
+      var (nx, ny) = (x + dx, y + dy);
+      if (!IsInBounds(nx, ny, height, width))
+        continue;
+      if (grid[ny][nx] == '@')
+        counter++;
+      if (counter >= 4)
+        break;
+    }
+    return counter;
+  }
+
+  private static bool IsInBounds(int x, int y, int height, int width) {
+    return x >= 0 && x < width && y >= 0 && y < height;
+  }
+
+  private static char[][] GetGrid(string[] data) {
+    var grid = new List<char[]>();
+    foreach (var row in data)
+      grid.Add(row.ToCharArray());
+    return grid.ToArray();
+  }
+}
---
+++ Day04/readme.md
@@ -0,0 +1,13 @@
+# Advent of Code 2025 #
+## Day 4: Printing Department ##
+https://adventofcode.com/2025/day/4
+
+File: sample.txt
+
+Part 1 Result: 13 in 1.0218ms\
+Part 2 Result: 43 in 0.6265ms
+
+File: input.txt
+
+Part 1 Result: 1604 in 2.1158ms\
+Part 2 Result: 9397 in 38.8693ms
