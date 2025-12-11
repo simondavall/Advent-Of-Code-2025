9977004ef5b3803a1d318abda7cb6232b5f26f3e
Simon Da Vall <simon.davall@gmail.com>
2025-12-11 15:24:31 +0000

    Day09

--- Day09/Solution.cs
+++ Day09/Solution.cs
@@ -10,15 +10,16 @@ internal static partial class Program
   private const long ExpectedPartTwo = 1343576598;
 
   private static PriorityQueue<((long x, long y) a, (long x, long y) b), long> maxAreas = new(Comparer<long>.Create((a, b) => b.CompareTo(a)));
+
   private static long PartOne(string data)
   {
-    var tiles = GetTiles(data);
-    for (var i = 0; i < tiles.Length; i++) {
-      var (ax, ay) = tiles[i];
-      for (var j = i + 1; j < tiles.Length; j++) {
+    var corners = GetCorners(data);
+    for (var i = 0; i < corners.Length; i++) {
+      var (ax, ay) = corners[i];
+      for (var j = i + 1; j < corners.Length; j++) {
         if (i == j)
           continue;
-        var (bx, by) = tiles[j];
+        var (bx, by) = corners[j];
         long area = (Math.Abs(ax - bx) + 1) * (Math.Abs(ay - by) + 1);
         maxAreas.Enqueue(((ax, ay), (bx, by)), area);
       }
@@ -30,92 +31,100 @@ internal static partial class Program
 
   private static long PartTwo(string data)
   {
-    var tiles = GetTiles(data);
-    var edges = CreateEdges(tiles);
-
+    var corners = GetCorners(data);
+    var (horizontalEdges, verticalEdges) = CreateEdges(corners);
     long maxArea;
     while (true) {
       if (!maxAreas.TryDequeue(out var item, out maxArea))
         break;
       var (a, b) = item;
 
-      if (IsContainedByPolygon(a, b, edges)) {
+      if (!RectIntersectsEdges(a, b, horizontalEdges, verticalEdges)) {
         break;
       }
     }
-
     return maxArea;
   }
 
-  private static bool IsContainedByPolygon((long x, long y) a, (long x, long y) b, HashSet<(long x, long y)> edges)
+  private static bool RectIntersectsEdges((long x, long y) a, (long x, long y) b, List<Edge> horizontalEdges, List<Edge> verticalEdges)
   {
+    // Normalize rectangle by making corners (top left) and (bottom right)
     if (a.y > b.y) (a.y, b.y) = (b.y, a.y);
     if (a.x > b.x) (a.x, b.x) = (b.x, a.x);
-    // search along top row for tiles and edges
-    var y = a.y + 1;
-    for (var x = a.x + 1; x < b.x; x++) {
-      if (edges.Contains((x, y))) {
-        return false;
-      }
-    }
-    // search along left column for tiles and edges
-    for (y = a.y + 2; y < b.y; y++) {
-      var x = a.x + 1;
-      if (edges.Contains((x, y))) {
-        return false;
-      }
-    }
-    // search along right column for tiles and edges
-    for (y = a.y + 2; y < b.y; y++) {
-      var x = b.x - 1;
-      if (edges.Contains((x, y))) {
-        return false;
-      }
-    }
-    // search along bottom row for tiles and edges
-    y = b.y - 1;
-    for (var x = a.x + 1; x < b.x; x++) {
-      if (edges.Contains((x, y))) {
-        return false;
-      }
-    }
 
-    return true;
+    // shrink rectangle by 1 to take inside edge of rectangle
+    a = (a.x + 1, a.y + 1);
+    b = (b.x - 1, b.y - 1);
+
+    // if top horizontal line intersects vertical edges return true
+    if (HasVerticalIntersect((a.x, a.y), (b.x, a.y), verticalEdges)) return true;
+    // if bottom horizontal line intersects vertical edges return true
+    if (HasVerticalIntersect((a.x, b.y), (b.x, b.y), verticalEdges)) return true;
+    // if left vertical line intersects horizontal edges return true
+    if (HasHorizontalIntersect((a.x, a.y), (a.x, b.y), horizontalEdges)) return true;
+    // if rigtt vertical line intersects horizontal edges return true
+    if (HasHorizontalIntersect((b.x, a.y), (b.x, b.y), horizontalEdges)) return true;
+
+    return false;
   }
 
-  private static HashSet<(long x, long y)> CreateEdges((long x, long y)[] tiles)
+  private static bool HasVerticalIntersect((long x, long y) a, (long x, long y) b, List<Edge> edges)
   {
-    var edges = new HashSet<(long, long)>();
-    for (var i = 0; i < tiles.Length - 1; i++) {
-      CreateEdge(tiles[i], tiles[i + 1], edges);
+    foreach (var edge in edges) {
+      if (a.x < edge.Start.x && b.x > edge.Start.x && a.y > edge.Start.y && a.y < edge.End.y) {
+        return true;
+      }
     }
-    CreateEdge(tiles[0], tiles[^1], edges);
-    return edges;
+    return false;
   }
 
-  private static void CreateEdge((long x, long y) a, (long x, long y) b, HashSet<(long x, long y)> edges)
+  private static bool HasHorizontalIntersect((long x, long y) a, (long x, long y) b, List<Edge> edges)
   {
-    if (a.x == b.x) {
-      if (a.y > b.y) (a.y, b.y) = (b.y, a.y);
-      for (var y = a.y; y <= b.y; y++) {
-        edges.Add((a.x, y));
+    foreach (var edge in edges) {
+      if (a.x > edge.Start.x && a.x < edge.End.x && a.y < edge.Start.y && b.y > edge.Start.y) {
+        return true;
       }
-    } else if (a.y == b.y) {
-      if (a.x > b.x) (a.x, b.x) = (b.x, a.x);
-      for (var x = a.x; x <= b.x; x++) {
-        edges.Add((x, a.y));
-      }
+    }
+    return false;
+  }
+
+  private class Edge((long, long) start, (long, long) end)
+  {
+    public (long x, long y) Start { get; set; } = start;
+    public (long x, long y) End { get; set; } = end;
+
+    public override string ToString()
+    {
+      return $"{Start}->{End}";
     }
   }
 
-  private static (long x, long y)[] GetTiles(string data)
+  private static (List<Edge> horizontal, List<Edge> vertical) CreateEdges((long x, long y)[] corners)
   {
-    var tiles = new List<(long, long)>();
+    List<Edge> verticalEdges = [];
+    List<Edge> horizontalEdges = [];
+    for (var i = 0; i < corners.Length; i++) {
+      var (ax, ay) = corners[i];
+      var (bx, by) = corners[(i + 1) % corners.Length];
+      // vertical or horizontal
+      if (ax == bx) // vertical
+      {
+        verticalEdges.Add(new Edge((ax, Math.Min(ay, by)), (bx, Math.Max(ay, by))));
+      } else {
+        horizontalEdges.Add(new Edge((Math.Min(ax, bx), ay), (Math.Max(ax, bx), by)));
+      }
+    }
+    return (horizontalEdges, verticalEdges);
+  }
+
+  private static (long x, long y)[] GetCorners(string data)
+  {
+    var corners = new List<(long, long)>();
 
     foreach (var line in data.Split('\n', StringSplitOptions.RemoveEmptyEntries)) {
       var (x, y) = line.Split(',').ToLongTuplePair();
-      tiles.Add((x, y));
+      corners.Add((x, y));
     }
-    return tiles.ToArray();
+    return corners.ToArray();
   }
 }
